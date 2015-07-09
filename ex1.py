3323a9cdf53a7fd9da3af4212c3de6ce436dd775
Alex Esparza <amdtek93@gmail.com>
2015-07-08 22:27:02 -0700

    This is a test

---
+++ ex1.py
@@ -0,0 +1,2 @@
+print 'This is a line'
+print 'This is a example of the print function'
---
+++ ex4.py
@@ -0,0 +1,15 @@
+my_name = 'Alex Esparza'
+my_age = 35 # a lie
+my_height = 75 # inches
+my_weight = 250 # pounds
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Black'
+
+print "Let's talk about %s." % my_name
+print "He's %d inches tall." % my_height
+print "He's %d pounds heavy." % my_weight
+print "Actually thats's not too heavy."
+print "He's got %s eyes and %s hair." % (my_eyes, my_hair)
+print "His teeth are usually %s depending on the coffee." % my_teeth
+print "If I add %d, %d, and %d I get %d." % (my_age, my_height, my_height, my_age + my_height + my_weight)
